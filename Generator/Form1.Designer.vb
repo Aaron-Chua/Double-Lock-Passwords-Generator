7262834361512fa92a4501ec5397dd5687c0b663
sonicprogrammer <sonichunter@sonichunter.tk>
2015-01-24 13:53:36 +1100

    Very first version.

---
+++ Generator/Form1.Designer.vb
@@ -0,0 +1,316 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(320, 56)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(187, 19)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Strongly disorder in output file"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"})
+        Me.ComboBox1.Location = New System.Drawing.Point(243, 54)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(63, 23)
+        Me.ComboBox1.TabIndex = 3
+        Me.ComboBox1.Text = "6"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(146, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Password length of lock A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(282, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(147, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Password length of lock B"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"})
+        Me.ComboBox2.Location = New System.Drawing.Point(243, 89)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(63, 23)
+        Me.ComboBox2.TabIndex = 4
+        Me.ComboBox2.Text = "6"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(80, 164)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(320, 21)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.TabStop = False
+        Me.TextBox1.Text = "Please select a target file from browse button."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 167)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Output file"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(408, 158)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 32)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 308)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(231, 29)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Password for lock A:"
+        Me.Label4.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 277)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(481, 28)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Generate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 127)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 388)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(232, 29)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Password for lock B:"
+        Me.Label6.Visible = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 337)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(65, 29)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "0000"
+        Me.Label7.Visible = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(12, 417)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(65, 29)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "0000"
+        Me.Label8.Visible = False
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"2", "3", "4", "5", "6", "7", "8", "9"})
+        Me.ComboBox3.Location = New System.Drawing.Point(165, 19)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(63, 23)
+        Me.ComboBox3.TabIndex = 1
+        Me.ComboBox3.Text = "3"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"2", "3", "4", "5", "6", "7", "8", "9"})
+        Me.ComboBox4.Location = New System.Drawing.Point(433, 19)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(63, 23)
+        Me.ComboBox4.TabIndex = 2
+        Me.ComboBox4.Text = "3"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 57)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(225, 15)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Nuber of candidate passwords for lock A"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(13, 92)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(226, 15)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Nuber of candidate passwords for lock B"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AcceptsReturn = True
+        Me.TextBox2.Location = New System.Drawing.Point(16, 215)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(481, 56)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(13, 197)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(232, 15)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "Password group description (for yourself):"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(312, 78)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(202, 60)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "Note:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Password length of lock A and B " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "MUST be same in strongly disorder " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "mode" & _
+    " (otherwise will be useless)."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(519, 455)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Double Lock Passwords Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox4 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+
+End Class
---
+++ Generator/Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As DialogResult = SaveFileDialog1.ShowDialog()
+        SaveFileDialog1.CheckPathExists = True
+        SaveFileDialog1.AutoUpgradeEnabled = True
+        If result = Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = SaveFileDialog1.FileName
+        End If
+    End Sub
+    Private Sub combin()
+        Dim possible As Integer
+        If CheckBox1.Checked = False Then
+            possible = Val(ComboBox1.Text) * Val(ComboBox2.Text)
+        Else
+            possible = arrangement(Val(ComboBox1.Text) + Val(ComboBox2.Text), 2)
+        End If
+        Label5.Text = "Number of possible password combination is : " + Str(possible)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call combin()
+    End Sub
+    Private Sub ComboBox1_TextChanged(sender As Object, e As EventArgs) Handles ComboBox1.TextChanged
+        Call combin()
+    End Sub
+    Private Sub ComboBox2_TextChanged(sender As Object, e As EventArgs) Handles ComboBox2.TextChanged
+        Call combin()
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Call combin()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim lockALength As Integer
+        Dim lockBLength As Integer
+        Dim lockACand As Integer
+        Dim lockBCand As Integer
+        Dim disorder As Boolean
+        Dim path As String
+        lockALength = Val(ComboBox3.Text)
+        lockBLength = Val(ComboBox4.Text)
+        lockACand = Val(ComboBox1.Text)
+        lockBCand = Val(ComboBox2.Text)
+        disorder = CheckBox1.Checked
+        path = TextBox1.Text
+        Dim chooseA As Integer
+        Dim chooseB As Integer
+        Dim temp As String
+        Label7.Text = ""
+        Label8.Text = ""
+        Label4.Visible = False
+        Label6.Visible = False
+        Label7.Visible = False
+        Label8.Visible = False
+        If lockALength > 9 Or lockALength < 1 Or lockBLength > 9 Or lockALength < 1 Then
+            MsgBox("Password length must between 1 to 9.", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error")
+        ElseIf lockACand > 50 Or lockACand < 1 Or lockBCand > 50 Or lockBCand < 1 Then
+            MsgBox("Number of candidate passwords must between 1 to 50 for each lock.", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error")
+        ElseIf TextBox1.Text = "Please select a target file from browse button." Then
+            MsgBox("No vaild output file specified.", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error")
+        Else
+            If Not disorder Then
+                Dim lockA(110, 10) As Integer
+                Dim lockB(110, 10) As Integer
+                Dim file As New FileStream(path, FileMode.Append)
+                Dim out As StreamWriter = New StreamWriter(file)
+                out.WriteLine(DateString + "  " + TimeString)
+                out.WriteLine("Description: " + TextBox2.Text)
+                out.WriteLine("====Lock A====")
+                out.WriteLine("No." + vbTab + vbTab + "password")
+                For j = 1 To lockACand
+                    temp = ""
+                    For i = 1 To lockALength
+                        Randomize(TimeOfDay.ToOADate())
+                        lockA(j, i) = CInt(Math.Ceiling(Rnd() * 9))
+                        temp = temp + Str(lockA(j, i)).Substring(1)
+                    Next
+                    out.WriteLine(Str(j) + vbTab + vbTab + temp)
+                Next
+                out.WriteLine("====Lock B====")
+                out.WriteLine("No." + vbTab + vbTab + "password")
+                For j = 1 To lockBCand
+                    temp = ""
+                    For i = 1 To lockBLength
+                        Randomize(TimeOfDay.ToOADate())
+                        lockB(j, i) = CInt(Math.Ceiling(Rnd() * 9))
+                        temp = temp + Str(lockB(j, i)).Substring(1)
+                    Next
+                    out.WriteLine(Str(j) + vbTab + vbTab + temp)
+                Next
+                Randomize(TimeOfDay.ToOADate())
+                chooseA = CInt(Math.Ceiling(Rnd() * lockACand - 1)) + 1
+                Randomize(TimeOfDay.ToOADate())
+                chooseB = CInt(Math.Ceiling(Rnd() * lockBCand - 1)) + 1
+                For i = 1 To lockALength
+                    Label7.Text = Label7.Text + Str(lockA(chooseA, i)).Substring(1)
+                Next
+                For i = 1 To lockBLength
+                    Label8.Text = Label8.Text + Str(lockB(chooseB, i)).Substring(1)
+                Next
+                out.WriteLine()
+                out.Close()
+                file.Close()
+                Label4.Visible = True
+                Label6.Visible = True
+                Label7.Visible = True
+                Label8.Visible = True
+            Else
+                If lockALength <> lockBLength Then
+                    MsgBox("Password length of lock A and B must be same in strongly disorder mode (otherwise will be useless).", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error")
+                Else
+                    Dim lock(110, 10) As Integer
+                    Dim file As New FileStream(path, FileMode.Append)
+                    Dim out As StreamWriter = New StreamWriter(file)
+                    out.WriteLine(DateString + "  " + TimeString)
+                    out.WriteLine("Description: " + TextBox2.Text)
+                    out.WriteLine("====Lock A and B====")
+                    out.WriteLine("No." + vbTab + vbTab + "password")
+                    For j = 1 To (lockACand + lockBCand)
+                        temp = ""
+                        For i = 1 To Math.Max(lockALength, lockBLength)
+                            Randomize(TimeOfDay.ToOADate())
+                            lock(j, i) = CInt(Math.Ceiling(Rnd() * 9))
+                            temp = temp + Str(lock(j, i)).Substring(1)
+                        Next
+                        out.WriteLine(Str(j) + vbTab + vbTab + temp)
+                    Next
+                    Randomize(TimeOfDay.ToOADate())
+                    chooseA = CInt(Math.Ceiling(Rnd() * (lockACand + lockBCand) - 1)) + 1
+                    Randomize(TimeOfDay.ToOADate())
+                    chooseB = CInt(Math.Ceiling(Rnd() * (lockACand + lockBCand) - 1)) + 1
+                    While chooseA = chooseB
+                        Randomize(TimeOfDay.ToOADate())
+                        chooseB = CInt(Math.Ceiling(Rnd() * (lockACand + lockBCand) - 1)) + 1
+                    End While
+                    For i = 1 To lockALength
+                        Label7.Text = Label7.Text + Str(lock(chooseA, i)).Substring(1)
+                    Next
+                    For i = 1 To lockBLength
+                        Label8.Text = Label8.Text + Str(lock(chooseB, i)).Substring(1)
+                    Next
+                    out.WriteLine()
+                    out.Close()
+                    file.Close()
+                    Label4.Visible = True
+                    Label6.Visible = True
+                    Label7.Visible = True
+                    Label8.Visible = True
+                End If
+            End If
+        End If
+    End Sub
+    Private Function arrangement(ByVal big As Integer, ByVal small As Integer) As Integer
+        Dim result As Integer
+        result = 1
+        For i = big - small + 1 To big
+            result = result * i
+        Next
+        Return result
+    End Function
+End Class
